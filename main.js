fe7065317c092fa1b1c97e8ea38fbd48cc87ec1d
czross <czr@uw.edu>
2016-04-12 23:04:07 -0700

    Second Graph
    The second graphic has been added

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
  */
 $(function() {
 
-    var bar1 = {
+    var barA1 = {
         x: ["Aerobacter aerogenes", "Brucella abortus", "Brucella anthracis", "Diplococcus pneumoniae",
         "Escherichia coli", "Klebsiella pneumoniae", "Mycobacterium tuberculosis", "Proteus vulgaris",
         "Pseudomonas aeruginosa", "Salmonella typhosa", "Salmonella schottmuelleri",
@@ -18,7 +18,7 @@ $(function() {
         }
     };
 
-    var bar2 = {
+    var barA2 = {
         x: ["Aerobacter aerogenes", "Brucella abortus", "Brucella anthracis", "Diplococcus pneumoniae",
             "Escherichia coli", "Klebsiella pneumoniae", "Mycobacterium tuberculosis", "Proteus vulgaris",
             "Pseudomonas aeruginosa", "Salmonella typhosa", "Salmonella schottmuelleri",
@@ -33,7 +33,7 @@ $(function() {
         }
     };
 
-    var bar3 = {
+    var barA3 = {
         x: ["Aerobacter aerogenes", "Brucella abortus", "Brucella anthracis", "Diplococcus pneumoniae",
             "Escherichia coli", "Klebsiella pneumoniae", "Mycobacterium tuberculosis", "Proteus vulgaris",
             "Pseudomonas aeruginosa", "Salmonella typhosa", "Salmonella schottmuelleri",
@@ -48,7 +48,7 @@ $(function() {
         }
     };
 
-    var data1 = [bar1, bar2, bar3];
+    var dataA = [barA1, barA2, barA3];
 
     var layout1 = {
        title: "Effectiveness of different antibacterials",
@@ -61,6 +61,46 @@ $(function() {
         barmode: 'group'
     };
 
-    Plotly.newPlot("Chart1", data1, layout1)
+    Plotly.newPlot("Chart1", dataA, layout1)
+
+    var dataB1 = {
+        x: [1, 2, 0.4, 1.2, 5, 0.1, 2, 0.4, 0.8],
+        y: [1.6, 0.02, 0.1, 1, 2, 0.1, 0.4, 0.008, 0.09],
+        mode: "markers",
+        name: "Negative Staining",
+        text: ["Aerobacter aerogenes", "Brucella abortus", "Escherichia coli", "Klebsiella pneumoniae",
+            "Mycobacterium tuberculosis", "Proteus vulgaris", "Pseudomonas aeruginosa",
+            "Salmonella typhosa", "Salmonella schottmuelleri"],
+        marker: {
+            size: 15,
+            color: "rgb(191, 11, 41)"
+        }
+    };
+
+    var dataB2 = {
+        x: [0.01, 11, 0.1, 0.03, 1, 14, 10],
+        y: [0.007, 10, 0.001, 0.001, 0.1, 10, 40],
+        mode: "markers",
+        name: "Positive Staining",
+        text: ["Brucella anthracis", "Diplococcus pneumoniae", "Staphylococcus albus", "Staphylococcus aureus",
+            "Streptococcus fecalis", "Streptococcus hemolyticus",
+            "Streptococcus viridans"],
+        marker: {
+            size: 15,
+            color: "rgb(11, 191, 59)"
+
+        }
+    };
+
+    var dataB = [dataB1, dataB2];
+
+    var layout2 = {
+        yaxis: {
+            type: "log"
+        },
+        title: "Relation between Gram Staining and effectiveness"
+    }
+
+    Plotly.newPlot("Chart2", dataB, layout2)
 
 });
